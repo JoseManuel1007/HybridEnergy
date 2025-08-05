81da0e3100a878dd24e2643e8fa9972bd34cb7bd
AlejandraMontoya <maria.montoyae@udea.edu.co>
2025-08-04 23:17:28 -0500

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,24 @@
+function calificar() {
+  const respuestas = {
+    q1: "B",
+    q2: "C",
+    q3: "C",
+    q4: "C",
+    q5: "F",
+    q6: "V",
+    q7: "F",
+    q8: "V"
+  };
+
+  let correctas = 0;
+
+  for (let key in respuestas) {
+    const seleccionada = document.querySelector(`input[name="${key}"]:checked`);
+    if (seleccionada && seleccionada.value === respuestas[key]) {
+      correctas++;
+    }
+  }
+
+  document.getElementById("resultado").innerHTML =
+    `Tuviste ${correctas} de 8 respuestas correctas.`;
+}
